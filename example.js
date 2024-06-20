75d31ad5392904df5c3404b01d9ca5145d7a7fc1
rileygoerg <rgoerg@usf.edu>
2024-06-19 21:18:37 -0400

    Initialize SVG container with specified dimensions.

---
+++ example.js
@@ -0,0 +1,16 @@
+function main() {
+   var barData = [100, 200, 300, 400]
+   var width = 500;
+   var scaleFactor = 10;
+   var barHeight = 20;
+
+   var graph = d3.select("body")
+   .append("svg")
+   .attr('width', width)
+   .attr('height', barHeight * barData.length);
+   
+   
+
+
+
+}
